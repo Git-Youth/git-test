e7104cc3b1b9e1adb65c48c5519905975a537042
fanhouxing <458621558@qq.com>
2019-09-27 20:05:37 +0800

    commit second

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
 public class UserController  {
+    public static void main(String[] args) {
+
+    }
 }
